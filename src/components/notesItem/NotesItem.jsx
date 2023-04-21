492e3286a6ff9e0671b19c4baaada1cb7f3f9fde
seva <lopachuk.seva@gmail.com>
2023-04-21 14:23:43 +0200

    add delete button title display changed

--- src/components/notesItem/NotesItem.jsx
+++ src/components/notesItem/NotesItem.jsx
@@ -1,10 +1,23 @@
+import IntfButton from '../ui/IntfButton';
 import classes from './NotesItem.module.css'
 
 
+
 const NotesItem = ({note}) => {
+
+    const getTitle = (note) => {
+        if (note.title.length > 50 ) {
+            return note.title.slice(0, 50) + '...';
+        } 
+
+        return note.title
+    }
+
     return (
         <div className={classes.wrapper}>
-            <p>{note.title.slice(0, 50)}</p>
+            <p>{getTitle(note)}</p>
+            <IntfButton>-</IntfButton>
+            
         </div>
     )
 }
