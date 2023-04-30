581cc99ed6f60dcbf17a204ce7aab07739caa173
seva <lopachuk.seva@gmail.com>
2023-04-30 22:24:56 +0200

    add logout link

--- src/components/basePage/BasePage.jsx
+++ src/components/basePage/BasePage.jsx
@@ -1,14 +1,22 @@
 import { Link } from 'react-router-dom'
 import classes from './BasePage.module.css'
+import { useContext } from 'react'
+import { AuthContext } from '../../providers/AuthProvider'
 
 
 const BasePage = ({ children }) => {
+
+    const {token} = useContext(AuthContext);
+
     return (
         <div className={classes.wrapper}>
             <header>
                 <ul className={classes.headerList}>
                     <li><Link to={'/notes'} className={`${classes.headerLink} ${classes.orange}`}>Notes App</Link></li>
-                    <li><Link to={'/login'} className={classes.headerLink}>Login</Link></li>
+                    {token ? <li><Link to={'/logout'} className={classes.headerLink}>Logout</Link></li>
+                    : <li><Link to={'/login'} className={classes.headerLink}>Login</Link></li>
+                    }
+                    
                 </ul>
             </header>
             <main>
